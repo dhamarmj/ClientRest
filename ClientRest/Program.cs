03bb7e5947188517563e862559ed91c1334a8029
Dhamar Martínez <39349434+dhamarmj@users.noreply.github.com>
2019-12-14 13:58:02 -0400

    subiendo

--- ClientRest/Program.cs
+++ ClientRest/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,20 @@ namespace ClientRest
     {
         static void Main(string[] args)
         {
+            HttpWebRequest webrequest = (HttpWebRequest)WebRequest.Create("http://localhost:4567/api/url/");
+            webrequest.Method = "GET";
+            webrequest.ContentType = "application/json";
+            Stream stream = webrequest.GetRequestStream();
+            stream.Close();
+            string result;
+            using (WebResponse response = webrequest.GetResponse()) 
+            {
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    result = reader.ReadToEnd();
+                    Console.WriteLine(Convert.ToString(result));
+                }
+            }
         }
     }
 }
